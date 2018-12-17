f6340df9aaad6eab4d4845fd5d8418c30fe35ae6
SulyvanD <sulyvan.debraize@net-c.com>
2018-12-17 14:47:50 +0100

    Exercice completé

---
+++ Variables - Exercice 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables___Exercice_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            a = a + 33;
+            b++;
+            c = a / b;
+        }
+    }
+}
